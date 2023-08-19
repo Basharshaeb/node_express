12acc28c8bc9f34dafc27a80876e7cedd51f56cc
Bashar Shaeb <sssbas228@gmail.com>
2023-08-19 21:59:21 +0300

    first commit

---
+++ app/models/index.js
@@ -0,0 +1,59 @@
+const dbConfig = require("../config/db.config.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
+module.exports = db;
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+db.comments = require("./comment.model.js")(sequelize, Sequelize);
+db.tag = require("./tag.model.js")(sequelize, Sequelize);
+
+db.tutorials.hasMany(db.comments, { as: "comments" });
+db.comments.belongsTo(db.tutorials, {
+  foreignKey: "tutorialId",
+  as: "tutorial",
+});
+
+
+db.tag.belongsToMany(db.tutorials, {
+  through: "tutorial_tag",
+  as: "tutorials",
+  foreignKey: "tag_id",
+});
+db.tutorials.belongsToMany(db.tag, {
+  through: "tutorial_tag",
+  as: "tags",
+  foreignKey: "tutorial_id",
+});
+// users and permissions 
+
+db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
+db.role.belongsToMany(db.user, {
+  through: "user_roles"
+});
+db.user.belongsToMany(db.role, {
+  through: "user_roles"
+});
+
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
---
+++ app/models/tutorial.model.js
@@ -0,0 +1,15 @@
+module.exports = (db, Sequelize) => {
+    const Tutorial = db.define("tutorial", {
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      published: {
+        type: Sequelize.BOOLEAN
+      }
+    });
+  
+    return Tutorial;
+  };
